f4cbf3440eae4587b56549663dbaeee26a7e79d5
btamega <bougarytamega77@gmail.com>
2022-03-20 10:04:09 +0000

    second commit

--- tuto/myapp/urls.py
+++ tuto/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('test', views.home,name='Home page'),
+    path('', views.home,name='Home page'),
     path('about/', views.about,name='About page'),
-    path('contact/', views.contact,name='About page'),
+    path('contact/', views.contact,name='Contact page'),
 ]
